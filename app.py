4cc213b90421490fa695a90484563547b7e8ecf9
Daniel Fernandez <danny.fdez99@gmail.com>
2020-11-30 16:50:00 -0500

    Added code to pack the info gathering frame.
    Now the info gathering frame has its labels, entries for data and the Done button that will call a method to fetch the info, no data validation yet.

--- app.py
+++ app.py
@@ -21,7 +21,8 @@ class App(tk.Tk):
 
 
 	def pack_initial_frame(self):
-		"""Creates the initial frame where player has two buttons to decide whether to create or join a game."""
+		"""Creates the initial frame where player has two buttons to decide whether to create or join a game.
+		A lambda funtion is used so that we can pass arguments into the called function or method."""
 
 		self.first_frame = tk.Frame(self)
 
@@ -40,7 +41,45 @@ class App(tk.Tk):
 		"""In this screen player has the entry widgets to provide info such as name, ip_address, and port. Depending
 		on the mode, the prompt to enter the ip_address and the port will vary."""
 
-		pass
+		if mode == 'create': #Player chose to create a game
+
+			ip_address_prompt = "Input your device ip address"
+			port_prompt = "Input the port to listen"
+
+		elif mode == 'join': #Player chose to join a gama
+
+			ip_address_prompt = "Input the server's ip address"
+			port_prompt = "Input the server's listening port"
+
+		else:
+
+			print("What just happened?")
+
+		self.gather_info_frame = tk.Frame(self)
+
+		self.prompt_name_label = tk.Label(self.gather_info_frame, text="Input your name:")
+		self.prompt_name_label.pack()
+		self.name_entry = tk.Entry(self.gather_info_frame)
+		self.name_entry.pack()
+
+		self.prompt_ip_address_label = tk.Label(self.gather_info_frame, text=ip_address_prompt)
+		self.prompt_ip_address_label.pack()
+		self.ip_address_entry = tk.Entry(self.gather_info_frame)
+		self.ip_address_entry.pack()
+
+		self.prompt_port_label = tk.Label(self.gather_info_frame, text=port_prompt)
+		self.prompt_port_label.pack()
+		self.port_entry = tk.Entry(self.gather_info_frame)
+		self.port_entry.pack()
+
+		#Depending on what the user chose to do, the creating or joining method will be called.
+		command = self.create_game if mode == 'create' else self.join_game
+
+		self.done_button = tk.Button(self.gather_info_frame, text="Done", command=command)
+		self.done_button.pack()
+
+		self.first_frame.pack_forget()
+		self.gather_info_frame.pack()
 
 
 	def create_game(self):
@@ -54,6 +93,17 @@ class App(tk.Tk):
 		"""Player connects to the server client, sends name over and creates the frame."""
 
 		pass
+
+
+	def gather_info(self):
+		"""Gets info from entries in self.gathering_info_frame and returns it. Port is converted to int so that it can 
+		be used later. Might add some input validation, and make sure all entries are filled."""
+
+		name = self.name_entry.get()
+		ip_address = self.ip_address_entry.get()
+		port = int(self.port_entry.get())
+
+		return name, ip_address, port
 
 
 if __name__ == "__main__":
